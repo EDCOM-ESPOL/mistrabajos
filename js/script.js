f176ae7487aabd2494424b76a6d71394d4b1c461
IvonneBurgos <irburgos@espol.edu.ec>
2016-10-19 14:22:19 -0500

    Estructura de presentacion de datos:

--- js/script.js
+++ js/script.js
@@ -12,14 +12,15 @@
 
 	$(document).ready(function () {
 
-		setInterval(ajaxRequest, 1000);
+		//setInterval(ajaxRequest, 1000);
 
-		$('#hello').click(function () {
-			alert('Hello from your script file');
+		$('#endJob').click(function () {
+			ajaxRequestDon();
 		});
 	});
 
-	function ajaxRequest(){
+	function ajaxRequestDon(){
+		var jobsArray = [];
 		var url = OC.generateUrl('/apps/mistrabajos/echo');
 			var data = {
 				type: "jobs"
@@ -28,15 +29,31 @@
 			$.post(url, data).success(function (response) {
 				completeData = JSON.parse(response.get);
 				var count = Object.keys(completeData.jobs).length;
-				var jobsArray = [];
-
 				for (var i = 0; i < count; i++) {
-						jobsArray.push(completeData.jobs[i].name+" "+completeData.jobs[i].state +" "+ completeData.jobs[i].blocks[0].p_percentage + "<br>");
-					
+						jobsArray.push("<div>" + completeData.jobs[i].name+" "+completeData.jobs[i].state +" "+ completeData.jobs[i].blocks[0].p_percentage+ "</div>");
 				}	
-
 				$('#echo-result').html(jobsArray);
 			});
+
+			//http://codepen.io/alassetter/full/cyrfB/
+	};
+
+	function ajaxRequestProcess(){
+		var jobsArray = [];
+		var url = OC.generateUrl('/apps/mistrabajos/echo');
+			var data = {
+				type: "jobs"
+			};
+
+			$.post(url, data).success(function (response) {
+				completeData = JSON.parse(response.get);
+				var count = Object.keys(completeData.jobs).length;
+				for (var i = 0; i < count; i++) {
+						jobsArray.push(completeData.jobs[i].name+" "+completeData.jobs[i].state +" "+ completeData.jobs[i].blocks[0].p_percentage);
+				}	
+				$('#echo-result').html(jobsArray);
+			});
+		
 	};
 
 })(jQuery, OC);
