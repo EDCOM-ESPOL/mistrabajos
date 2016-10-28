e330bf4c99ad8b3cc98b7b8df0021aa1810a48b6
IvonneBurgos <irburgos@espol.edu.ec>
2016-10-28 11:02:55 -0500

    Mensaje de retroalimentación

--- js/script.js
+++ js/script.js
@@ -13,12 +13,16 @@
 	$(document).ready(function () {
         
 		var timer = false;
-		ajaxRequestDon();    
+		ajaxRequestDon();
+
         $('#endJob').attr("class", "selectedOption");
+
 		$('#endJob').click(function () {
+			//$('#processjobmessage').hide();
+			//$('#endjobmessage').show();
 			$('#endJob').attr("class", "selectedOption");
 			$('#processJob').attr("class", " ");
-			ajaxRequestDon();
+			  ajaxRequestDon();
 			if (timer !== false){
 				clearInterval(timer);
 				timer = false;
@@ -26,15 +30,13 @@
 		});
 
 		$('#processJob').click(function () {
+			$('#bigCont').empty();
 			$('#processJob').attr("class", "selectedOption");
 			$('#endJob').attr("class", " ");
 			timer = setInterval(ajaxRequestProcess, 1000);
 		});
 		
 	});
-
-
-	
 
 	function ajaxRequestDon(){
 
@@ -44,7 +46,6 @@
 				type: "jobs"
 			};
 			$.post(url, data).success(function (response) {
-				//$('.table').remove();
 				$('#bigCont').empty();
 				var listnum=0;
 				var completeData = JSON.parse(response.get);
@@ -77,13 +78,16 @@
 			    var completeData = JSON.parse(response.get);
 				var count = Object.keys(completeData.jobs).length;
 				for (var i = 0; i < count; i++) {
-					if (completeData.jobs[i].state !== " DON" && completeData.jobs[i].user_name === variable/*$('#expandDisplayName').text()*/){
+					if (completeData.jobs[i].state !== " DON" && completeData.jobs[i].user_name === $('#userNameFront').text()){
 						    listnum+=1;
 					    	var utcSeconds = completeData.jobs[i].time_started;
 							var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
 							d.setUTCSeconds(utcSeconds);
 							var date = formatDate(d);
 								$('#bigCont').append('<div class="panel panel-default"><div class="panel-body"><div class="panel body-head"><h3>'+completeData.jobs[i].name+'</h3><span class="label label-primary">Renderizando...</span></div><div class="panel body-info"><div class="panel row-info"><p class="info-label">Fecha: </p><p class="info-text">'+date+'</p></div><div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: '+completeData.jobs[i].blocks[0].p_percentage+'%;">'+completeData.jobs[i].blocks[0].p_percentage+'%</div></div></div></div></div>');
+						}
+					else {
+							$('#bigCont').html('<div id="endjobmessage" class="alert alert-info"><strong>Info!</strong> No hay trabajos por descargar.</div>');
 						}
 					}	
 			});
