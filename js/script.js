c2c3cc479cb0c5c63800c7edf408795682859e20
master <you@example.com>
2016-11-02 13:04:23 -0500

    Visor Completo

--- js/script.js
+++ js/script.js
@@ -49,11 +49,8 @@
 	});
 
 	function ajaxRequestDon(){
-
-
  		$('#processjobmessage').hide();
 		var jobsArray = { "data" :[]};
-		//var jobsArray =[];
 		var url = OC.generateUrl('/apps/mistrabajos/echo');
 			var data = {
 				type: "jobs"
@@ -72,39 +69,49 @@
 							var date = formatDate(d);
 							var percentage = "<div class='progress-bar' role='progressbar' aria-valuenow= '60' aria-valuemin='0' aria-valuemax='100' style='width:100%;'>"+ completeData.jobs[i].blocks[0].p_percentage+"%</div>";
 							jobsArray.data.push({ "listnum":listnum, "name":completeData.jobs[i].name, "date": date, "percentage":percentage});					
-					}	
+					}
 				}
-				addData(jobsArray.data.sort(comp));
+				if(listnum >= 1){
+					$('#endjobmessage').hide();
+					addData(jobsArray.data.sort(comp));
+				}
+				else {
+					$('#endjobmessage').show();
+				}
 			});
 		};
 
 	function ajaxRequestProcess(){
-
+        $('#endjobmessage').hide();
 		var jobsArray = [];
 		var url = OC.generateUrl('/apps/mistrabajos/echo');
-			var data = {
-				type: "jobs"
-			};
-			$.post(url, data).done(function (response) {
-				$('#bigCont').empty();
-				var listnum=0;
-			    var completeData = JSON.parse(response.get);
-				var count = Object.keys(completeData.jobs).length;
-				for (var i = 0; i < count; i++) {
-					if (completeData.jobs[i].state !== " DON" && completeData.jobs[i].user_name === $('#userNameFront').text()){
-						    listnum+=1;
-					    	var utcSeconds = completeData.jobs[i].time_started;
-							var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-							d.setUTCSeconds(utcSeconds);
-							var date = formatDate(d);
-							    $('#processjobmessage').hide();
-								$('#bigCont').append('<div class="panel panel-default"><div class="panel-body"><div class="panel body-head"><h3>'+completeData.jobs[i].name+'</h3><span class="label label-primary">Renderizando...</span></div><div class="panel body-info"><div class="panel row-info"><p class="info-label">Fecha: </p><p class="info-text">'+date+'</p></div><div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: '+completeData.jobs[i].blocks[0].p_percentage+'%;">'+completeData.jobs[i].blocks[0].p_percentage+'%</div></div></div></div></div>');
-						}
-					else {
-						$('#processjobmessage').show();	
-						}
-					}	
-			});
+		var data = {
+			type: "jobs"
+		};
+		$.post(url, data).done(function (response) {
+			$('#bigCont').empty();
+			var listnum=0;
+			var completeData = JSON.parse(response.get);
+			var count = Object.keys(completeData.jobs).length;
+			for (var i = 0; i < count; i++) {
+				if ((completeData.jobs[i].state === " RDY RUN" && completeData.jobs[i].user_name === $('#userNameFront').text()) || (completeData.jobs[i].state === " RDY" && completeData.jobs[i].user_name === $('#userNameFront').text())){
+					listnum+=1;
+					var utcSeconds = completeData.jobs[i].time_started;
+					var d = new Date(0);
+					d.setUTCSeconds(utcSeconds);
+					var date = formatDate(d);
+					jobsArray.push('<div class="panel panel-default"><div class="panel-body"><div class="panel body-head"><h3>'+completeData.jobs[i].name+'</h3><span class="label label-primary">Renderizando...</span></div><div class="panel body-info"><div class="panel row-info"><p class="info-label">Fecha: </p><p class="info-text">'+date+'</p></div><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: '+completeData.jobs[i].blocks[0].p_percentage+'%;">'+completeData.jobs[i].blocks[0].p_percentage+'%</div></div></div></div></div>');
+				}
+			}
+
+			if(listnum >= 1){
+				$('#processjobmessage').hide();
+				$('#bigCont').html(jobsArray);
+			}
+			else {
+				$('#processjobmessage').show();
+			}
+		});
 	};
 
 	function formatDate(d){
@@ -128,7 +135,6 @@
 	};
 
 	function comp(a, b) {
-
 		var aa = a.date.split('/').reverse().join(),
         bb = b.date.split('/').reverse().join();
     return bb < aa ? -1 : (bb > aa ? 1 : 0);
